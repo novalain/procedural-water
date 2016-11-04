85fff8123589970725fef5aa16ac3eb557321440
novalain <michael.f.noven@gmail.com>
2016-11-04 21:41:23 +0100

    Add plane under spinning cube as wireframe

--- app/js/app.js
+++ app/js/app.js
@@ -22,9 +22,21 @@ class Application {
   }
 
   setupScene_() {
+    this.scene = new THREE.Scene();
+    //this.bufferScene = new THREE.Scene();
+    //this.bufferTexture = new THREE.WebGLRenderTarget(
+    //    window.innerWidth, window.innerHeight,
+    //    {minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter});
+
     const light = new THREE.DirectionalLight(0xffffff);
     light.position.set(0, 1, 1).normalize();
-    const geometry = new THREE.BoxGeometry(200, 200, 200);
+    const geometry = new THREE.BoxGeometry(100, 100, 100);
+
+    const waterMaterial = new THREE.MeshBasicMaterial(
+        {color: 0x26ff26, side: THREE.Backside, wireframe: true});
+    const planeGeometry = new THREE.PlaneGeometry(400, 400, 3, 3);
+    const waterMesh = new THREE.Mesh(
+        planeGeometry,waterMaterial);
 
     //const geometry = new THREE.PlaneGeometry(150, 300, 50);
     //const material = new THREE.MeshBasicMaterial({color: 0x000000});
@@ -34,14 +46,17 @@ class Application {
       specular: 0x555555,
       shininess: 30,
     });
-
     //var material = new THREE.MeshPhongMaterial( { map: THREE.ImageUtils.loadTexture('js/crate.jpg') } );
     //const material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-
-    this.scene = new THREE.Scene();
     this.mesh = new THREE.Mesh(geometry, material);
+    this.mesh.position.y += 80;
+    waterMesh.position.y -= 50;
+    waterMesh.rotation.x = Math.PI * 3 / 4;
     this.scene.add(light);
+    //this.bufferScene.add(this.mesh);
+    this.scene.add(waterMesh);
     this.scene.add(this.mesh);
+
   }
 
   onResize_(e) {
@@ -56,6 +71,10 @@ class Application {
     this.mesh.rotation.x += 0.005;
     this.mesh.rotation.y += 0.01;
 
+    //this.mainBoxObject.rotation.x += 0.005;
+    //this.mainBoxObject.rotation.y += 0.01;
+
+    //this.renderer.render(this.bufferScene, this.camera, this.bufferTexture);
     this.renderer.render(this.scene, this.camera);
     requestAnimationFrame(this.loop.bind(this));
   }
