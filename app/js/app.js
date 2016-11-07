a42e3ac95f6ea05e348bc881086601b0a67ac853
novalain <michael.f.noven@gmail.com>
2016-11-07 17:21:15 +0100

    Implement simplex noise on plane using Stegu library

--- app/js/app.js
+++ app/js/app.js
@@ -14,11 +14,14 @@ class Application {
     this.camera = new THREE.PerspectiveCamera(60, this.WIDTH / this.HEIGHT, 1, 5000);
     this.camera.position.set(0, 0, 500);
     this.renderer = new THREE.WebGLRenderer({
-      alpha: true,
+    //  alpha: true,
       antialias: true,
     });
+    //this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setClearColor(0x000000);
     this.renderer.setSize(this.WIDTH, this.HEIGHT);
     this.container.appendChild(this.renderer.domElement);
+    this.time = 0;
   }
 
   listen_() {
@@ -44,21 +47,24 @@ class Application {
     light.position.set(0, 1, 1).normalize();
     const geometry = new THREE.BoxGeometry(100, 100, 100);
 
+    this.waterUniforms = { time: { value: 0 } };
     const waterMaterial = new THREE.ShaderMaterial({
       vertexShader: document.getElementById('vertexShader').textContent,
       fragmentShader: document.getElementById('fragmentShader').textContent,
-      wireframe:true,
+      uniforms : this.waterUniforms,
+      side: THREE.DoubleSide,
     });
+
     const planeGeometry = new THREE.PlaneGeometry(400, 400, 1, 1);
     const waterMesh = new THREE.Mesh(planeGeometry, waterMaterial);
-    const material = new THREE.MeshPhongMaterial({
+    const cubeMaterial = new THREE.MeshPhongMaterial({
       color: 0x0033ff,
       specular: 0x555555,
       shininess: 30,
     });
 
     /// TODO: Keep all scene objects that needs to be updated in a list
-    this.cubeMesh = new THREE.Mesh(geometry, waterMaterial);
+    this.cubeMesh = new THREE.Mesh(geometry, cubeMaterial);
     this.cubeMesh.position.y += 80;
     waterMesh.position.y -= 50;
     waterMesh.rotation.x =  Math.PI*3/4;
@@ -71,9 +77,7 @@ class Application {
   loop() {
     this.cubeMesh.rotation.x += 0.005;
     this.cubeMesh.rotation.y += 0.01;
-
-    //this.mainBoxObject.rotation.x += 0.005;
-    //this.mainBoxObject.rotation.y += 0.01;
+    this.waterUniforms.time.value += 0.05;
 
     //this.renderer.render(this.bufferScene, this.camera, this.bufferTexture);
     this.renderer.render(this.scene, this.camera);
